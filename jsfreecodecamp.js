ce8bace78ffb1c3a6cff24d3b3adc63e3079c8c6
Georgia <mmeh28@yahoo.ro>
2022-08-02 11:02:57 +0300

    javascript basics

---
+++ jsfreecodecamp.js
@@ -0,0 +1,140 @@
+//1
+//my first comment
+
+/* my first
+long
+comment */
+
+//2
+var a;
+a = 7;
+var b;
+b=a;
+
+//3
+var c = 9;
+
+//4
+var myFirstName = 'Georgia';
+var myLastName = 'Capilnar';
+
+//5
+var a=5;
+var b=10;
+var c='I am a';
+
+//6
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+//7
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+//8
+let catName = "Oliver";
+let catSound = "Meow!";
+
+//9
+const FCC = "freeCodeCamp"; 
+let fact = "is cool!"; 
+fact = "is awesome!";
+console.log(FCC, fact); 
+
+//10
+const sum = 10 + 10;
+
+//11
+const difference = 45 - 33;
+
+//12
+const product = 8 * 10;
+
+//13
+const quotient = 66 / 33;
+
+//14
+let myVar = 87;
+myVar++;
+
+//15
+let myVar2 = 11;
+myVar--;
+
+//16
+const ourDecimal = 5.7;
+const myDecimal =3.4;
+
+//17
+const product2 = 2.0 * 2.5;
+
+//18
+const quotient2 = 4.4 / 2.0;
+
+//19
+const remainder = 11% 3;
+
+//20
+let a = 3;
+let b = 17;
+let c = 12;
+a += 12;
+b += 9;
+c +=7;
+
+//21
+let a = 11;
+let b = 9;
+let c = 3;
+a -=6;
+b -=15;
+c -=1;
+
+//22
+let a = 5;
+let b = 12;
+let c = 4.6;
+a *= 5;
+b *=3;
+c *=10;
+
+//23
+let a = 48;
+let b = 108;
+let c = 33;
+a /= 12;
+b /= 4;
+c /=  11;
+
+//24
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; 
+
+//25
+const myStr2 = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+//26
+const myStr3 = "FirstLine\n\t\\\SecondLine\nThirdLine";
+
+//27
+const myStr4 = "This is the start. " + "This is the end.";
+
+//28
+let myStr5 = "This is the first sentence. ";
+myStr5 += "This is the second sentence.";
+
+//29
+const myName = "Georgia";
+const myStr6 = "My name is " + myName + "and I am well";
+
+//30
+const someAdjective = "wow";
+let myStr7 = "Learning to code is ";
+myStr += someAdjective;
